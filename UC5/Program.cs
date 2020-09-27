3aa03fd59faa6e82889016ee971d1c45b355d6ca
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-09-28 00:11:49 +0530

    Added

---
+++ UC5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caluclating month wages");
+            int hrsprsnt = 8, wage = 20, days = 20;
+            int monthwage;
+            monthwage = hrsprsnt * wage * days;
+            Console.WriteLine("Monthly wage: {0}",monthwage);
+        }
+    }
+}
