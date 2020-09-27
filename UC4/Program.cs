8afd2d7098e5ff16facff61b4e09b940d5fe9c8a
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-09-27 23:12:23 +0530

    Added

---
+++ UC4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random R1 = new Random();
+            Random R2 = new Random();
+            int emp = R1.Next(1, 3);
+            int b = R2.Next(1, 3);
+            switch (emp) {
+                case 1:
+                    {
+                        Console.WriteLine("Present");
+                        switch (b)
+                        {
+                            case 1: Console.WriteLine("Parttime employee,wage=80"); break;
+                            case 2: Console.WriteLine("Fulltime employee,wage=160"); break;
+                        }
+                        break;
+                    }
+                case 2: 
+                        Console.WriteLine("Absent");break;
+                    
+            } 
+            }
+        }
+    }
+
